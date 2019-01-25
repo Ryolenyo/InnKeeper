a00ed667f91c8983fc27b60aadb6f037503a1e52
Ryolenyo <ryolenyo@gmail.com>
2019-01-25 21:14:51 +0700

    Add Node to Node

---
+++ Assets/Script/moveFromNodeToNode.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class moveFromNodeToNode : MonoBehaviour
+{
+
+    public Transform NowNode;
+    public Transform DestNode;
+
+    public int speed = 2;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (transform.position != DestNode.position){
+            transform.position = Vector2.MoveTowards(transform.position,DestNode.position,Time.deltaTime*speed);
+        }
+    }
+}

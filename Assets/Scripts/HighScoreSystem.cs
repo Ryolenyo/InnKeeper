e0c2c9c5ed776e239cc1df80d0e509212daa8c47
Pattnatt <jittapat007@gmail.com>
2019-01-26 07:49:45 +0700

    Implement Highscore System

---
+++ Assets/Scripts/HighScoreSystem.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HighScoreSystem : MonoBehaviour
+{
+    private static string playerPrefKey = "HighScoreTest";
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public static int LoadHighScore()
+    {
+        if (!PlayerPrefs.HasKey(playerPrefKey))
+        {
+            PlayerPrefs.SetInt(playerPrefKey, 0);
+        }
+
+        return PlayerPrefs.GetInt(playerPrefKey);
+    }
+
+    public static bool SaveHighScore(int score)
+    {
+        bool result = score > LoadHighScore();
+
+        if (result)
+        {
+            PlayerPrefs.SetInt(playerPrefKey, score);
+        }
+
+        return result;
+    }
+}
---
+++ Assets/Scripts/Test/HighScoreTester.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HighScoreTester : MonoBehaviour
+{
+    public int currentScore;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("HighScoreTester: Current Highscore = " + HighScoreSystem.LoadHighScore());
+        Debug.Log("HighScoreTester: Current Score = " + currentScore);
+        if(HighScoreSystem.SaveHighScore(currentScore))
+        {
+            Debug.Log("HighScoreTester: New Highscore");
+        }
+        Debug.Log("HighScoreTester: Current Highscore = " + HighScoreSystem.LoadHighScore());
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}

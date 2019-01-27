8edb8b6b534bc59e381a9c136d95d4ec435cfda7
Pattnatt <jittapat007@gmail.com>
2019-01-27 08:06:19 +0700

    GameUI

---
+++ Assets/GameUiManager.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameUiManager : MonoBehaviour
+{
+    public GameObject[] gameOverlay;
+    public GameObject[] resultOverlay;
+
+    [Header("Text")]
+    public Text scoreText;
+    public Text timerText;
+    public Text resultHighscoreText;
+    public Text resultScoreText;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        foreach(GameObject gameObject in resultOverlay)
+        {
+            gameObject.SetActive(false);
+        }
+
+        GameTracker.OnTimerDone += OnTimeOut;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        scoreText.text = GameTracker.GetCurrentScore().ToString();
+        timerText.text = (Mathf.CeilToInt(GameTracker.GetTimeLeft())).ToString();
+    }
+
+    private void OnDestroy()
+    {
+        GameTracker.OnTimerDone -= OnTimeOut;
+    }
+
+    private void OnTimeOut()
+    {
+        foreach (GameObject gameObject in resultOverlay)
+        {
+            gameObject.SetActive(true);
+        }
+
+        foreach (GameObject gameObject in gameOverlay)
+        {
+            gameObject.SetActive(false);
+        }
+
+        resultScoreText.text = GameTracker.GetCurrentScore().ToString();
+        resultHighscoreText.text = HighScoreSystem.LoadHighScore().ToString();
+    }
+}
